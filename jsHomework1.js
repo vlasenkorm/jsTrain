89d3f7b69d6d7697b1963e904ee10179dd8d3259
vlasenkorm <vlasenkorm@gmail.com>
2019-03-19 19:24:16 +0200

    Homework JS 1 5 Task.

---
+++ jsHomework1.js
@@ -0,0 +1,41 @@
+//Task1    
+    const topRowKeyboardEng = "qwertyuiop[]";
+    console.log(topRowKeyboardEng);
+    const midleRowKeyboardEng = "asdfghjkl;'";
+    console.log(midleRowKeyboardEng);
+    const bottomRowKeyboardEng = "zxcvbnm,./";
+    console.log(bottomRowKeyboardEng);
+    
+//Task2
+    const lengthTopRowKeyEng = topRowKeyboardEng.length;
+    console.log(lengthTopRowKeyEng);
+    const lengthMidRowKeyEng = midleRowKeyboardEng.length;
+    console.log(lengthMidRowKeyEng);
+    const lengthBotRowKeyEng = bottomRowKeyboardEng.length;
+    console.log(lengthBotRowKeyEng);
+    
+//Task3
+//console.log(topRowKeyboardEng.charAt(0) + topRowKeyboardEng.charAt(--lengthTopRowKeyEng));???????
+    
+    console.log(topRowKeyboardEng.charAt(0) + topRowKeyboardEng.charAt(lengthTopRowKeyEng - 1));
+    console.log(typeof(lengthTopRowKeyEng));
+    console.log(midleRowKeyboardEng.charAt(0) + midleRowKeyboardEng.charAt(lengthMidRowKeyEng - 1));
+    console.log(bottomRowKeyboardEng.charAt(0) + bottomRowKeyboardEng.charAt(lengthBotRowKeyEng - 1));
+
+//Task4
+    
+    const positionCharOpenSB = topRowKeyboardEng.indexOf("[");
+    console.log(positionCharOpenSB);
+    const positionCharCloseSB = topRowKeyboardEng.indexOf("]");
+    console.log(positionCharCloseSB);
+
+//Task5
+
+    console.log(`topRowKeyboardEng = ${topRowKeyboardEng} 
+lengthTopRowKeyEng = ${lengthTopRowKeyEng}
+midleRowKeyboardEng = ${midleRowKeyboardEng}
+lengthMidRowKeyEng = ${lengthMidRowKeyEng}
+bottomRowKeyboardEng = ${bottomRowKeyboardEng}
+lengthBotRowKeyEng = ${lengthBotRowKeyEng}
+positionCharOpenSB = ${positionCharOpenSB}
+positionCharCloseSB = ${positionCharCloseSB}` );
